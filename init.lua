ce95c0c488eb397f569c6313119a073d3782eb38
octacian <enduffy2014@outlook.com>
2017-03-05 11:30:02 -0800

    Add /hudplus chatcommand
    Returns HUD Plus version. Will later be expanded to allow configuring HUD Plus.

--- init.lua
+++ init.lua
@@ -2,6 +2,9 @@
 
 hudplus = {}
 
+hudplus.VERSION = 0.1
+hudplus.RELEASE = "alpha"
+
 local modpath = minetest.get_modpath("hudplus")
 
 -- [function] Log
@@ -10,6 +13,18 @@ function hudplus.log(content, log_type)
   if log_type == nil then log_type = "action" end
   minetest.log(log_type, "[HUD Plus] "..content)
 end
+
+---------------------
+---- CHATCOMMAND ----
+---------------------
+
+-- [register cmd] /hudplus
+minetest.register_chatcommand("hudplus", {
+  description = "Manage HUD Plus",
+  func = function(name)
+    return true, "HUD Plus: Version "..tostring(hudplus.VERSION).." ("..hudplus.RELEASE..")"
+  end,
+})
 
 -------------------
 ----- MODULES -----
