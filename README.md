3cec50a436b8b2b80e87c865943540c9d151f7f5
octacian <enduffy2014@outlook.com>
2017-03-05 10:58:47 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+HUD Plus [hudplus]
+==================
+
+This mod aims to create a better player HUD and modding API. README will be updated as features are added.
---
+++ init.lua
@@ -0,0 +1,60 @@
+-- hudplus/init.lua
+
+hudplus = {}
+
+local modpath = minetest.get_modpath("hudplus")
+
+-- [function] Log
+function hudplus.log(content, log_type)
+  if not content then return false end
+  if log_type == nil then log_type = "action" end
+  minetest.log(log_type, "[HUD Plus] "..content)
+end
+
+-------------------
+----- MODULES -----
+-------------------
+
+local loaded_modules = {}
+
+local settings = Settings(modpath.."/modules.conf"):to_table()
+
+-- [function] Get module path
+function hudplus.get_module_path(name)
+  local module_path = modpath.."/modules/"..name
+
+  if io.open(module_path.."/init.lua") then
+    return module_path
+  end
+end
+
+-- [function] Load module (overrides modules.conf)
+function hudplus.load_module(name)
+  if loaded_modules[name] ~= false then
+    local module_init = hudplus.get_module_path(name).."/init.lua"
+
+    if module_init then
+      dofile(module_init)
+      loaded_modules[name] = true
+      return true
+    else
+      hudplus.log("Invalid module \""..name.."\". The module either does not exist "..
+        "or is missing an init.lua file.", "error")
+    end
+  else
+    return true
+  end
+end
+
+-- [function] Require module (does not override modules.conf)
+function hudplus.require_module(name)
+  if settings[name] and settings[name] ~= false then
+    return hudplus.load_module(name)
+  end
+end
+
+for name,enabled in pairs(settings) do
+  if enabled ~= false then
+    hudplus.load_module(name)
+  end
+end
